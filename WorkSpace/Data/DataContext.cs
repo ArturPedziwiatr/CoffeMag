22d1406333dfb9af2727cf7c0b71e5414e3476a3
ArturPedziwiatr <95591012+ArturPedziwiatr@users.noreply.github.com>
2022-04-05 12:05:58 +0200

    Dodaj pliki projektów.

---
+++ WorkSpace/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿#nullable disable
+using Microsoft.EntityFrameworkCore;
+using WorkSpace.Entities;
+
+namespace WorkSpace.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WorkSpace/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkSpace.Entities
+{
+    public class Product
+    {
+        public Guid Id { get;  set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        public int? Number { get; set; } 
+        public int? Quantity { get; set; }
+        [StringLength(200)]
+        public string? Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
